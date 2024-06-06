cf3821fa15f963eed989f6b628850a8992ec3fc5
AbdulMoiz5702 <abdulmoizkoikenx10@gmail.com>
2024-06-06 16:48:21 +0500

    backgorung_fixed

--- lib/all_screens.dart
+++ lib/all_screens.dart
@@ -1,54 +1,91 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:testing_sticker/stickr_pack_screen.dart';
-import 'package:testing_sticker/upload_stickers.dart';
 
 class AllStickersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          Icons.add,
-          color: Colors.grey,
+        appBar: AppBar(
+          title: Text('All Sticker Packs'),
         ),
+        body: StreamBuilder(
+            stream: FirebaseFirestore.instance.collection('packs').snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
+              var packs = snapshot.data!.docs;
+              return ListView.builder(
+                itemCount: packs.length,
+                itemBuilder: (context, index) {
+                  var pack = packs[index];
+                  return StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('packs')
+                        .doc(pack.id)
+                        .collection('stickers')
+                        .limit(5) // Limit to first 5 stickers
+                        .snapshots(),
+                    builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (!snapshot.hasData) {
+                        return ListTile(
+                          onTap: (){
 
-
-
-        onPressed: (){
-          Navigator.of(context).push(CupertinoPageRoute(builder: (ctx)=> UploadStickerScreen()));
-        },
-      ),
-      appBar: AppBar(
-        title: Text('All Sticker Packs'),
-      ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('packs').snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) {
-            return Center(child: CircularProgressIndicator());
-          }
-          var packs = snapshot.data!.docs;
-          return ListView.builder(
-            itemCount: packs.length,
-            itemBuilder: (context, index) {
-              var pack = packs[index];
-              return ListTile(
-                title: Text(pack['name']),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => StickerPackScreen(packId: pack.id,packName: pack['name'],),
-                    ),
+                          },
+                          title: Text(pack['name']),
+                          subtitle: Text('Loading...'),
+                        );
+                      }
+                      var stickers = snapshot.data!.docs;
+                      return InkWell(
+                        onTap: (){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => StickerPackScreen(packId: pack.id,packName: pack['name'],),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          height: 250,
+                          width: MediaQuery.sizeOf(context).width * 1,
+                          child:
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(pack['name']),
+                              SingleChildScrollView(
+                                scrollDirection: Axis.horizontal,
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: List.generate(stickers.length, (index){
+                                    var sticker = stickers[index];
+                                    return Column(
+                                      children: [
+                                        Image.network(
+                                          sticker['image_url'],
+                                          height: 100,
+                                          width: 100,
+                                        ),
+                                        SizedBox(height: 8.0),
+                                        Text(sticker['name']),
+                                      ],
+                                    );
+                                  }),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
                   );
                 },
               );
             },
-          );
-        },
-      ),
-    );
-  }
+            ),
+        );
+    }
 }
+
--- lib/stickr_pack_screen.dart
+++ lib/stickr_pack_screen.dart
@@ -1,11 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_editor_plus/options.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_editor_plus/image_editor_plus.dart';
-import 'package:flutter/services.dart';
+import 'package:background_remover/background_remover.dart';
+import 'package:image/image.dart' as img;
 
 class StickerPackScreen extends StatefulWidget {
   final String packId;
@@ -18,6 +20,20 @@ class StickerPackScreen extends StatefulWidget {
 }
 
 class _StickerPackScreenState extends State<StickerPackScreen> {
+  Future<void> _uploadSticker(Uint8List imageData) async {
+    String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+    Reference storageRef = FirebaseStorage.instance.ref().child('stickers/$fileName');
+    UploadTask uploadTask = storageRef.putData(imageData);
+    TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
+    String imageUrl = await taskSnapshot.ref.getDownloadURL();
+
+    await FirebaseFirestore.instance.collection('packs').doc(widget.packId).collection('stickers').add({
+      'name': 'stickerName',
+      'image_url': imageUrl,
+    });
+  }
+
+
   List<Uint8List?> _imageDataList = List.generate(30, (index) => null);
 
   void _showBottomSheet(BuildContext context, int index) {
@@ -26,14 +42,49 @@ class _StickerPackScreenState extends State<StickerPackScreen> {
       builder: (context) {
         return BottomSheetContent(
           packId: widget.packId,
-          onImageSelected: (Uint8List imageData) {
-            setState(() {
-              _imageDataList[index] = imageData;
-            });
+          onImageSelected: (Uint8List imageData) async {
+            // Remove background and resize to 400x400
+            Uint8List? imageWithoutBackground = await _removeBackground(imageData);
+            if (imageWithoutBackground != null) {
+              setState(() {
+                _imageDataList[index] = imageWithoutBackground;
+              });
+              Navigator.pop(context);
+              _openImageEditor(context, imageWithoutBackground, index);
+            }
           },
         );
       },
     );
+  }
+
+  Future<Uint8List?> _removeBackground(Uint8List imageData) async {
+    try {
+      // Remove background
+      Uint8List? result = await removeBackground(imageBytes: imageData);
+
+      if (result != null) {
+        // Decode the image
+        img.Image? image = img.decodeImage(result);
+        if (image != null) {
+          // Resize image to 400x400
+          img.Image resizedImage = img.copyResize(image, width: 750, height: 750);
+
+          // Encode the image back to Uint8List
+          return Uint8List.fromList(img.encodePng(resizedImage));
+        }
+      }
+      return null;
+    } catch (e) {
+      print("Error removing background: $e");
+      return null;
+    }
+  }
+
+  Uint8List _resizeImage(Uint8List data, int width, int height) {
+    img.Image? image = img.decodeImage(data);
+    img.Image resized = img.copyResize(image!, width: width, height: height);
+    return Uint8List.fromList(img.encodePng(resized));
   }
 
   Future<void> _openImageEditor(BuildContext context, Uint8List imageData, int index) async {
@@ -45,13 +96,17 @@ class _StickerPackScreenState extends State<StickerPackScreen> {
           cropOption: const CropOption(
             reversible: false,
           ),
+          emojiOption: EmojiOption(),
         ),
       ),
     );
 
     if (editedImageData != null) {
+      // Resize after editing
+      Uint8List resizedImageData = _resizeImage(editedImageData, 512, 512);
       setState(() {
-        _imageDataList[index] = editedImageData;
+        _imageDataList[index] = resizedImageData;
+        _uploadSticker(resizedImageData);
       });
     }
   }
@@ -118,21 +173,7 @@ class _BottomSheetContentState extends State<BottomSheetContent> {
     if (pickedFile != null) {
       Uint8List imageData = await pickedFile.readAsBytes();
       widget.onImageSelected(imageData);
-      Navigator.pop(context);
     }
-  }
-
-  Future<void> _uploadSticker(Uint8List imageData) async {
-    String fileName = DateTime.now().millisecondsSinceEpoch.toString();
-    Reference storageRef = FirebaseStorage.instance.ref().child('stickers/$fileName');
-    UploadTask uploadTask = storageRef.putData(imageData);
-    TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
-    String imageUrl = await taskSnapshot.ref.getDownloadURL();
-
-    await FirebaseFirestore.instance.collection('packs').doc(widget.packId).collection('stickers').add({
-      'name': 'stickerName',
-      'image_url': imageUrl,
-    });
   }
 
   @override
@@ -151,34 +192,6 @@ class _BottomSheetContentState extends State<BottomSheetContent> {
           title: Text('Open Gallery'),
           onTap: () {
             _pickImage(ImageSource.gallery);
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.insert_drive_file),
-          title: Text('Select File'),
-          onTap: () {
-            // Implement file selection logic
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.library_books),
-          title: Text('Select Library'),
-          onTap: () {
-            // Implement library selection logic
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.text_fields),
-          title: Text('Text Only'),
-          onTap: () {
-            // Implement text only logic
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.animation),
-          title: Text('Select Animated Sticker'),
-          onTap: () {
-            // Implement animated sticker selection logic
           },
         ),
       ],
